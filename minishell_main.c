b33f9c16f1323be84abc22f7964e5968ad659edf
Abdelouahed  Rabiai <abdell.rabiai@gmail.com>
2023-03-11 19:27:24 +0100

    fixed leaks export and environement leaks

--- minishell_main.c
+++ minishell_main.c
@@ -6,7 +6,7 @@
 /*   By: arabiai <arabiai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 12:24:53 by ahmaymou          #+#    #+#             */
-/*   Updated: 2023/03/11 16:42:01 by arabiai          ###   ########.fr       */
+/*   Updated: 2023/03/11 19:14:21 by arabiai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 void loo(void)
 {
 	system("leaks minishell");
+}
+
+char *get_envp_value(char *variable_name, t_infos *infos)
+{
+	t_envp *tmp;
+
+	tmp = infos->my_envp;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->variable_name, variable_name) == 0)
+				return (tmp->variable_value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
 
 int main(int ac, char **av, char **envp)
@@ -26,41 +40,47 @@ int main(int ac, char **av, char **envp)
 	t_infos infos;
 	init(&infos);
 	duplicate_envp(envp, &infos);
-	export(&infos);
+	// export(&infos);
+	// env(&infos);
+	// export_variable(&infos, "Tabi3a");
+	// export_variable(&infos, "Tabi3a=AI researcher");
+	// export_variable(&infos, "Tabi3a+=_something great");
+	// export(&infos);
+	// printf("thiers {%s}\n",getenv("Tabi3a"));
+	// printf("mine {%s}\n",get_envp_value("Tabi3a", &infos));
+	
+	// export_variable(&infos, "Tabi3a=1");
+	// export_variable(&infos, "Tabi3a=2");
+	// export_variable(&infos, "Tabi3a=3");
+	// export_variable(&infos, "Tabi3a=4");
+	// export_variable(&infos, "Tabi3a=5");
+	// export_variable(&infos, "Tabi3a=dfaa3 sdaar haz rass o fuck l3alaam tkoon m9iwaad");
+	// export_variable(&infos, "Tabi3a=opps");
+	// export_variable(&infos, "Tab");
+	// export_variable(&infos, "Tab=12");
+	// export_variable(&infos, "Black=1004");
+	// export_variable(&infos, "Black+=/Security researcher");
+	// export_variable(&infos, "*Black=");
+	// export_variable(&infos, "");
+	// printf("\n*--------------AFTER UNSET PATH-----------------*\n");
+	// unset("Tabi3a", &infos);
+	// unset("COLORTERM", &infos);
+	// unset("BZaytona", &infos);
+	// unset("Black", &infos);
+	// unset("PATH", &infos);
+	// unset("Apple_PubSub_Socket_Render", &infos);
+	// unset("__CF_USER_TEXT_ENCODING", &infos);
+	// unset("_P9K_TTY", &infos);
+	// unset("ZSH", &infos);
+	// printf("\n*--------------EXPORT-----------------*\n");
+	// printf("\n*--------------ENV-----------------*\n");
+	// export(&infos);
+	// printf("\n*--------------AFTER ENV-----------------*\n");
 	env(&infos);
-	export_variable(&infos, "Tabi3a");
-	export_variable(&infos, "Tabi3a=1");
-	export_variable(&infos, "Tabi3a=2");
-	export_variable(&infos, "Tabi3a=3");
-	export_variable(&infos, "Tabi3a=4");
-	export_variable(&infos, "Tabi3a=5");
-	export_variable(&infos, "Tabi3a=dfaa3 sdaar haz rass o fuck l3alaam tkoon m9iwaad");
-	export_variable(&infos, "Tabi3a=opps");
-	export_variable(&infos, "Tab");
-	export_variable(&infos, "Tab=12");
-	export_variable(&infos, "Black=1004");
-	export_variable(&infos, "Black+=/Security researcher");
-	export_variable(&infos, "*Black=");
-	export_variable(&infos, "");
-	printf("\n*--------------AFTER UNSET PATH-----------------*\n");
-	unset("Tabi3a", &infos);
-	unset("COLORTERM", &infos);
-	unset("BZaytona", &infos);
-	unset("Black", &infos);
-	unset("PATH", &infos);
-	unset("Apple_PubSub_Socket_Render", &infos);
-	unset("__CF_USER_TEXT_ENCODING", &infos);
-	unset("_P9K_TTY", &infos);
-	unset("ZSH", &infos);
-	printf("\n*--------------EXPORT-----------------*\n");
-	printf("\n*--------------ENV-----------------*\n");
-	export(&infos);
-	printf("\n*--------------AFTER ENV-----------------*\n");
-	env(&infos);
-	echo("ps\n", 0);
-	pwd();
-	cd("*");
-	pwd();
+	// echo("ps\n", 0);
+	// pwd();
+	// cd("*");
+	// pwd();
 	ft_free_envp(&infos.my_envp);
 	// while (1);
 }
